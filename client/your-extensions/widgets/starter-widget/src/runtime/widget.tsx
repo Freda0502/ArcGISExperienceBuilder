8bb8df3ce2fe09a89b3f577f2d5be3ad71cf24b1
Freda0502 <68989706+Freda0502@users.noreply.github.com>
2021-06-18 13:10:07 -0500

    created a starter widget

---
+++ client/your-extensions/widgets/starter-widget/src/runtime/widget.tsx
@@ -0,0 +1,15 @@
+/** @jsx jsx */
+import { React, AllWidgetProps, jsx } from 'jimu-core'
+
+export default class Widget extends React.PureComponent<
+  AllWidgetProps<any>,
+  any
+> {
+  render() {
+    return (
+      <div className='widget-starter jimu-widget'>
+        This is your starter widget!
+      </div>
+    )
+  }
+}
